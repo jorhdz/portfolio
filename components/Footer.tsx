7cd534b42695c389445765b8df499b4ed082ae5a
Jorge Hdz <johg.0792@gmail.com>
2023-02-23 18:12:24 -0600

    Inicio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
     <footer className="center mt-5 flex justify-center space-x-4 bg-[#E7E8EF] p-4 text-xs">
-      <p>Powered by Next.js, Salesforce Commerce Cloud, and Vercel </p>
-      <span>|</span>
+      <p>Envíame un correo:  </p>
+     
       <a
-        href="https://github.com/vercel/next.js/tree/canary/examples/with-sfcc"
+        href="mailto:johg.0792@gmail.com"
         className="font-medium text-orange-600"
       >
-        Source code
+       johg.0792@gmail.com
       </a>
     </footer>
   )
